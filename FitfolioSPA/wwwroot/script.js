9df5fb2c27edc0ef2417bd549392e3790da34a6a
DanIsStillHere <146327001+DanIsStillHere@users.noreply.github.com>
2024-04-14 23:42:04 +0100

    Day 5

--- FitfolioSPA/wwwroot/script.js
+++ FitfolioSPA/wwwroot/script.js
@@ -1,4 +1,10 @@
 ﻿$(document).ready(function () {
+    // Load routines from local storage if available
+    var storedRoutines = localStorage.getItem("routines");
+    var allRoutines = storedRoutines ? JSON.parse(storedRoutines) : [];
+    var storedInitRoutines = localStorage.getItem("initRoutines");
+    var initialRoutines = storedInitRoutines ? JSON.parse(storedInitRoutines) : [];
+
     // Homepage content HTML //
     var homeContent = `
         <div class="content">
@@ -17,13 +23,16 @@
             <form id="add-routine-form">
                 <label for="routine-name">Routine Name:</label>
                 <input type="text" id="routine-name" required>
-                <button type="submit">Search</button>
-                <button type="submit" id="create-routine">Create</button>
+                <button type="button" id="search-routine">Search</button>
+                <button type="button" id="create-routine">Create</button>
             </form>
             <div id="exercise-list">
                 <!-- Exercise list will be displayed here -->
             </div>
             <button id="save-routine">Save</button>
+            <button id="delete-routine">Delete</button>
+            <button id="edit-routine">Edit</button>
+            <button id="compare-routines">Compare</button>
         </div>
     `;
 
@@ -42,9 +51,6 @@
             <p>This is the readings section.</p>
         </div>
     `;
-
-    // Array to store routines //
-    var allRoutines = [];
 
     // Function to handle navigation clicks using event delegation //
     $(document).on("click", ".nav-link", function (e) {
@@ -87,18 +93,18 @@
         var routine = []; // Define routine outside the event listeners to maintain its scope //
 
         // Add event listener for the "Create" button //
-        $("#add-routine-form").on("submit", function (e) {
+        $("#create-routine").on("click", function (e) {
             // Prevent form submission //
             e.preventDefault();
 
             // Extract routine name from the form input //
             var routineName = $("#routine-name").val();
 
-            // Prompt the user to input the number of exercises //
-            var numExercises = parseInt(prompt("How many exercises do you wish to add?"));
-
             // Clear routine before adding new exercises //
             routine = [];
+
+            // Prompt the user to input the number of exercises //
+            var numExercises = parseInt(prompt("How many exercises do you wish to add?"));
 
             // Loop to input exercises //
             for (var i = 0; i < numExercises; i++) {
@@ -120,36 +126,149 @@
             $("#save-routine").prop("disabled", false);
         });
 
+        // Add event listener for the "Search" button //
+        $("#search-routine").on("click", function (e) {
+            // Prevent form submission //
+            e.preventDefault();
+
+            // Extract routine name from the form input //
+            var routineName = $("#routine-name").val();
+
+            // Search for the routine in the stored routines //
+            var foundRoutine = allRoutines.find(routine => routine.name === routineName);
+
+            if (foundRoutine) {
+                // Display the exercises of the found routine //
+                displayExercises(foundRoutine.exercises);
+            } else {
+                // Alert the user if the routine couldn't be found //
+                alert("Routine '" + routineName + "' not found.");
+            }
+        });
+
         // Add event listener for the "Save" button //
         $("#save-routine").on("click", function () {
             // Extract routine name from the form input //
             var routineName = $("#routine-name").val();
-            alert("Routine '" + routineName + "' and its contents have been saved.");
 
-            // Create JSON object for the current routine //
-            var routineObject = {
-                name: routineName,
-                exercises: routine
-            };
+            // Check if a routine with the same name already exists //
+            var existingRoutine = allRoutines.find(routine => routine.name === routineName);
 
-            // Add the current routine to the array of all routines //
-            allRoutines.push(routineObject);
+            if (existingRoutine) {
+                // Alert the user if a routine with the same name already exists //
+                alert("A routine with the name '" + routineName + "' already exists. Please choose a different name.");
+            } else {
+                // Proceed to save the routine //
 
-            // Convert the array of all routines to JSON string //
-            var jsonString = allRoutines.map(routine => JSON.stringify(routine)).join("\n");
+                // Create JSON object for the current routine //
+                var routineObject = {
+                    name: routineName,
+                    exercises: routine
+                };
 
-            // Create a blob with the JSON string //
-            var blob = new Blob([jsonString], { type: "application/json" });
+                // Add the current routine to the arrays for all and initial routines //
+                allRoutines.push(routineObject);
+                initialRoutines.push(routineObject);
 
-            // Create a link element to download the JSON file //
-            var a = document.createElement("a");
-            a.href = URL.createObjectURL(blob);
-            a.download = "routines.json";
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
+                // Save the updated routines to local storage //
+                localStorage.setItem("routines", JSON.stringify(allRoutines));
+                localStorage.setItem("initRoutines", JSON.stringify(initialRoutines));
+
+                // Alert the user that the routine has been saved //
+                alert("Routine '" + routineName + "' and its contents have been saved.");
+            }
+        });
+
+        // Add event listener for the "Delete" button //
+        $("#delete-routine").on("click", function () {
+            // Extract routine name from the form input //
+            var routineName = $("#routine-name").val();
+
+            // Search for the routine in the 'routines' and 'initRoutines' files //
+            var foundIndex = allRoutines.findIndex(routine => routine.name === routineName);
+            var initFoundIndex = initialRoutines.findIndex(routine => routine.name === routineName);
+
+            if (foundIndex !== -1 && initFoundIndex !== -1) {
+                // Ask the user if they want to delete the routine //
+                var confirmDelete = confirm("Are you sure you want to delete the routine '" + routineName + "'?");
+
+                if (confirmDelete) {
+                    // Delete the routine //
+                    allRoutines.splice(foundIndex, 1);
+                    initialRoutines.splice(initFoundIndex, 1);
+
+                    // Save the updated routines to local storage //
+                    localStorage.setItem("routines", JSON.stringify(allRoutines));
+                    localStorage.setItem("initRoutines", JSON.stringify(initialRoutines));
+
+                    // Alert the user that the routine has been deleted //
+                    alert("Routine '" + routineName + "' has been deleted.");
+                }
+            } else {
+                // Alert the user if no routine with that name is found //
+                alert("No routine with the name '" + routineName + "' is saved.");
+            }
+        });
+
+        // Add event listener for the "Edit" button //
+        $("#edit-routine").on("click", function () {
+            // Extract routine name from the form input //
+            var routineName = $("#routine-name").val();
+
+            // Check if the routine exists in 'routines' file //
+            var existingRoutineIndex = allRoutines.findIndex(routine => routine.name === routineName);
+            if (existingRoutineIndex !== -1) {
+                // Routine found, prompt user to update information //
+                var updatedRoutine = [];
+                var numExercises = parseInt(prompt("How many exercises do you wish to update?"));
+                for (var i = 0; i < numExercises; i++) {
+                    var exerciseName = prompt("Enter the name of exercise " + (i + 1));
+                    var exerciseSetRange = prompt("Enter the set range for exercise " + (i + 1));
+                    var exerciseRepRange = prompt("Enter the rep range for exercise " + (i + 1));
+                    var exercise = new Exercise(exerciseName, exerciseSetRange, exerciseRepRange);
+                    updatedRoutine.push(exercise);
+                }
+
+                // Update routine in 'routines' file //
+                allRoutines[existingRoutineIndex].exercises = updatedRoutine;
+
+                // Display updated exercises on the page //
+                displayExercises(updatedRoutine);
+
+                // Save updated routines to local storage //
+                localStorage.setItem("routines", JSON.stringify(allRoutines));
+            } else {
+                // Routine not found //
+                alert("No routine with the name '" + routineName + "' found.");
+            }
+        });
+
+        // Add event listener for the 'Compare' button //
+        $("#compare-routines").on("click", function () {
+            // Extract routine name from the form input //
+            var routineName = $("#routine-name").val();
+
+            // Check if routine exists in 'routines' file //
+            var existingRoutineIndex = allRoutines.findIndex(routine => routine.name === routineName);
+            if (existingRoutineIndex !== -1) {
+                // Routine found in 'routines' file //
+                var existingRoutine = allRoutines[existingRoutineIndex];
+                displayExercises(existingRoutine.exercises);
+            } else {
+                // Check if routine exists in 'initialRoutines' file //
+                var initialRoutineIndex = initialRoutines.findIndex(routine => routine.name === routineName);
+                if (initialRoutineIndex !== -1) {
+                    // Routine found in 'initialRoutines' file //
+                    var initialRoutine = initialRoutines[initialRoutineIndex];
+                    displayExercises(initialRoutine.exercises);
+                } else {
+                    // Routine not found //
+                    alert("No routine with the name '" + routineName + "' found.");
+                }
+            }
         });
     }
+
 
     // Function to display exercises on the page //
     function displayExercises(routine) {
